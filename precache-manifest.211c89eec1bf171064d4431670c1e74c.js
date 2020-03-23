3b7963fde2577e13d883c4b8a77684560ffaf0da
Adam m <adam@floorsmtihllc.com>
2020-03-23 00:07:05 -0400

    Updates

---
+++ precache-manifest.211c89eec1bf171064d4431670c1e74c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831162875eab4e160c3343afe25a42ca",
+    "url": "/fun-quiz-app/index.html"
+  },
+  {
+    "revision": "c657b74c4aa07e7956ba",
+    "url": "/fun-quiz-app/static/css/main.a13b0272.chunk.css"
+  },
+  {
+    "revision": "0216a5344d6cd3e02b70",
+    "url": "/fun-quiz-app/static/js/2.eef2eaef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fun-quiz-app/static/js/2.eef2eaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c657b74c4aa07e7956ba",
+    "url": "/fun-quiz-app/static/js/main.fb758fb6.chunk.js"
+  },
+  {
+    "revision": "d05083ade2a4c88399d3",
+    "url": "/fun-quiz-app/static/js/runtime-main.7f97571b.js"
+  }
+]);
